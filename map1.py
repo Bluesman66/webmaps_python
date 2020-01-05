dd8ff202aa9b964ace1b69043dad421480b183b8
Bluesman66 <alex.paclin@gmail.com>
2020-01-05 05:16:23 +0500

    108 Adding Multiple Markers to the Map

--- map1.py
+++ map1.py
@@ -1,14 +1,17 @@
 import folium
 
-map = folium.Map(location=[55.199100, 61.327461], zoom_start=16)
+map = folium.Map(location=[55.199100, 61.327461], zoom_start=12)
 fg = folium.FeatureGroup(name="My Group")
-fg.add_child(
-    folium.Marker(
-        location=[55.199100, 61.327461],
-        popup="Hi, there!", 
-        icon=folium.Icon(color="red")
+
+for coordinates in [[55.189100, 61.227461], [55.199100, 61.327461]]:
+    fg.add_child(
+        folium.Marker(
+            location=coordinates,
+            popup="Hi, there!",
+            icon=folium.Icon(color="red")
+        )
     )
-)
+
 map.add_child(fg)
 
 map.save("map1.html")
