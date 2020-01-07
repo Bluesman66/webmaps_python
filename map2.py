6755c20542d9a6bcbee39dcfed1830148396f31d
Bluesman66 <alex.paclin@gmail.com>
2020-01-07 21:03:49 +0500

    110 Creating Popup Windows for Map Features

--- map2.py
+++ map2.py
@@ -4,15 +4,16 @@ import pandas
 data = pandas.read_csv("Volcanoes_USA.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[42.8799019, -113.2210007], zoom_start=5)
 fg = folium.FeatureGroup(name="My Group")
 
-for lt, ln in zip(lat, lon):
+for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(
         folium.Marker(
             location=[lt, ln],
-            popup="Hi, there!",
+            popup=str(el) + "m",
             icon=folium.Icon(color="red")
         )
     )
